4e651e7b413b2cb2dc8bda0bb84ed3742d62560e
Vishnu Asokan <vishnuEIN@gmail.com>
2020-05-24 02:12:46 +0530

    Updates

---
+++ precache-manifest.d96d30793ee64f3683ca559c68bb2276.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac95ca26c4c2a3d8ab8a0a559308feac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c24c0af469b2b2d7366",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd031ddb8ef7d536b7e5",
+    "url": "/static/js/2.8f372af6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f372af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c24c0af469b2b2d7366",
+    "url": "/static/js/main.3b38714a.chunk.js"
+  },
+  {
+    "revision": "99721de78eb3e2206fa5",
+    "url": "/static/js/runtime-main.3f926080.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
