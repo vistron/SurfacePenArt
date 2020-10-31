bc1ba7bb2f69f3174929d92e581c48d3da6aa837
Vishnu Asokan <vishnu.a@brillio.com>
2020-10-31 17:53:24 +0530

    [DEV]: formatted portrait file

--- src/view/Portrait.js
+++ src/view/Portrait.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 
 class Portrait extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			items: [
-                {
-                    image: '',
-                    date: '2020-02-20'
-                },
-                {
-                    image: '',
-                    date: '2020-02-20'
-                },
-                {
-                    image: '',
-                    date: '2020-02-22'
-                }
-            ]
-		};
-	}
-	render() {
-		return (<div className="content toolbarSpace">
-            {this.state.items.map(item => {
-                return (
-                <div>{item.image}</div>
-                );
-            })}
-        </div>);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          image: '',
+          date: '2020-02-20'
+        },
+        {
+          image: '',
+          date: '2020-02-20'
+        },
+        {
+          image: '',
+          date: '2020-02-22'
+        }
+      ]
+    };
+  }
+  render() {
+    return (<div className="content toolbarSpace">
+      {this.state.items.map(item => {
+        return (
+          <div>{item.image}</div>
+        );
+      })}
+    </div>);
+  }
 }
 
 export default Portrait;
